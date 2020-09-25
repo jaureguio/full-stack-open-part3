804fea73ee650254a0d5f13325ac170f9691c06a
Oscar <oscarjaureguia@gmail.com>
2020-09-25 19:33:52 -0400

    feat(part3): solutions 3.2-8, backend allows basic CRUD functionality to the phonebook

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
-const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 
 const PORT = process.env.PORT
 
-const persons = [
+let persons = [
   {
     name: "Dan Abramov",
     number: "567056-5675",
@@ -32,22 +32,90 @@ const persons = [
   },
 ]
 
+morgan.token('body', (req, res) => {
+  if(req.method !== 'POST') return null
+  return `${JSON.stringify(req.body)}`
+})
+
+const loggerFormat = (tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    tokens.body(req, res),
+  ].join(" ");
+};
+
 const app = express()
 
 app
-  .get('/info', (req, res) => {
-    const phonebookSize = persons.length
-    const time = new Date()
+  .use(express.json())
+  .use(morgan(loggerFormat));
+
+app.get('/info', (req, res) => {
+  const phonebookSize = persons.length
+  const time = new Date()
+  
+  const template = `
+  <p>Phonebook has info for ${phonebookSize} people</p>
+  <p>${time}</p>
+  `
+  res.send(template)
+})
+
+app
+  .route("/api/persons/:id")
+  .get((req, res) => {
+    const reqId = Number(req.params.id)
+
+    const person = persons.find(({ id }) => id === reqId)
+
+    if (!person) {
+      return res.status(404).json({ error: "No person found" })
+    }
+
+    return res.json(person)
+  })
+  .delete((req, res) => {
+    const reqId = Number(req.params.id)
+
+    persons = persons.filter(({ id }) => id !== reqId)
+
+    return res.json({ message: 'Person deleted!' })
+  })
+
+app
+  .route("/api/persons")
+  .get((req, res) => {
+    return res.json(persons)
+  })
+  .post((req, res) => {
+    const newEntry = req.body
+
+    if(!newEntry.name || !newEntry.number) {
+      return res.status(400).json({ error: 'person name or number missing'})
+    }
+
+    const isUnique = persons.some(({ name }) => name === newEntry.name)
+
+    if(isUnique) {
+      return res.status(403).json({ error: 'name must be unique'})
+    }
+
+    const generateId = (length = 100000) => {
+      return length + Math.floor(Math.random()*length)
+    }
+
+    newEntry.id = generateId()
+    persons.push(newEntry)
     
-    const template = `
-    <p>Phonebook has info for ${phonebookSize} people</p>
-    <p>${time}</p>
-    `
-    res.send(template)
+    return res.status(201).json({ message: `${newEntry.name} was added!`})
   })
-  .get('/api/persons', (req, res) => {
-    res.json(persons)
-  })
-  .listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`)
-  })
+
+app.listen(PORT, () => {
+ console.log(`App is listening on port: ${PORT}`)
+})
